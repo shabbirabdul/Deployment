9fb6b697c549e180e012ab0e3b494b4ee89e4645
Shabbir Abdul <sabdul@ncsu.edu>
2015-04-06 10:54:42 -0400

    git hoot setup and data migration

--- infrastructure.js
+++ infrastructure.js
@@ -2,36 +2,79 @@ var http      = require('http');
 var httpProxy = require('http-proxy');
 var exec = require('child_process').exec;
 var request = require("request");
+var redis = require('redis')
 
-var GREEN = 'http://127.0.0.1:5060';
-var BLUE  = 'http://127.0.0.1:9090';
+var GREEN = 'http://localhost:3000';
+var BLUE  = 'http://localhost:3001';
+var TARGET = BLUE;
 
-var TARGET = GREEN;
 
-var infrastructure =
-{
-  setup: function()
-  {
-    // Proxy.
+var blue_redis = redis.createClient(6379, '127.0.0.1', {}) // client 1
+var green_redis = redis.createClient(6380, '127.0.0.1', {}) //client 2
+
+var flag = process.argv.slice(2)[0];
+
+var infrastructure ={
+  setup: function(){
     var options = {};
     var proxy   = httpProxy.createProxyServer(options);
+    var server  = http.createServer(function(req, res){
+      console.log(req.url);
+      if(req.url == '/switch'){
+        if(TARGET == BLUE){
+          TARGET=GREEN //toggle
+          console.log('Green Server');
+          blue_redis.lrange("images",0,-1,function(err,value){
+            value.forEach(function(data){
+            green_redis.lpush("images",data);
+            });
+          });
+        }
+        else{
+          TARGET = BLUE //toggle
+          console.log('Blue Server');
+          green_redis.lrange("images",0,-1,function(err,value){
+            value.forEach(function(data){
+            blue_redis.lpush("images",data);
+            });
+          });
+        }
+          res.statusCode=200;
+          res.write('switch over');
+          res.end();
+      }
+      else{
+              if(flag == 1){
+		            if(req.url == '/upload'){
+                    var route_to = '';
+                    if(TARGET == BLUE){
+                      route_to = GREEN
+                    }
+                    else{
+                      route_to = BLUE
+                    }
+                    req.pipe(request.post(route_to+'/upload'));
+                    proxy.web( req, res, {target: TARGET } );
+              	  }
+		          }
+              else{
+                proxy.web( req, res, {target: TARGET } );
+              }
+      }
 
-    var server  = http.createServer(function(req, res)
-    {
-      proxy.web( req, res, {target: TARGET } );
     });
-    server.listen(8080);
+    server.listen(8181);
 
     // Launch green slice
-    exec('forever start deploy/blue-www/main.js 9090');
+    exec('forever start blue-www/server1.js 3001 6379');
     console.log("blue slice");
 
     // Launch blue slice
-    exec('forever start deploy/green-www/main.js 5060');
+    exec('forever start green-www/server1.js 3000 6380');
     console.log("green slice");
 
 //setTimeout
-//var options = 
+//var options =
 //{
 //  url: "http://localhost:8080",
 //};
